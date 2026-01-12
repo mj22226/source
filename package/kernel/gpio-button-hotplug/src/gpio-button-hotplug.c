707e6330efce64032cd0d5ec00067399bc3feaaa
Marty Jones <mj8263788@gmail.com>
2026-01-12 20:52:51 -0300

    kernel: add linux 6.19-rc
    Signed-off-by: Marty Jones <mj8263788@gmail.com>

--- package/kernel/gpio-button-hotplug/src/gpio-button-hotplug.c
+++ package/kernel/gpio-button-hotplug/src/gpio-button-hotplug.c
@@ -64,7 +64,7 @@ struct gpio_keys_button_data {
 	const struct gpio_keys_button *b;
 };
 
-extern u64 uevent_next_seqnum(void);
+
 
 #define BH_MAP(_code, _name)		\
 	{				\
@@ -168,7 +168,7 @@ static int button_hotplug_fill_event(struct bh_event *event)
 	if (ret)
 		return ret;
 
-	ret = bh_event_add_var(event, 0, "SEQNUM=%llu", uevent_next_seqnum());
+
 
 	return ret;
 }
@@ -191,7 +191,7 @@ static void button_hotplug_work(struct work_struct *work)
 		goto out_free_skb;
 
 	NETLINK_CB(event->skb).dst_group = 1;
-	broadcast_uevent(event->skb, 0, 1, GFP_KERNEL);
+
 
  out_free_skb:
 	if (ret) {
@@ -515,7 +515,7 @@ static int gpio_keys_button_probe(struct platform_device *pdev,
 				/* or the legacy (button->gpio is good) way? */
 				error = devm_gpio_request_one(dev,
 					button->gpio, GPIOF_IN | (
-					button->active_low ? GPIOF_ACTIVE_LOW :
+					button->active_low ? GPIOD_OUT_LOW :
 					0), desc);
 				if (error) {
 					dev_err_probe(dev, error,
